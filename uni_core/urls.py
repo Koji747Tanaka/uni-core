584085f909329de2697f8a91e92f99b03f11713d
Kou747Tanaka <kou747tanaka@gmail.com>
2024-01-03 15:22:31 +1100

    [A] Establish connection with the video-management front

--- uni_core/urls.py
+++ uni_core/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("stream/", include("video_streaming.urls")),
+    path("api/files/", include("video_streaming.urls")),
     path("admin/", admin.site.urls),
 ]
